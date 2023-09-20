855ccd4cbe5d16c7ed58a5999b750b100ee92925
Jakub Łanoszka <jakublanoszka@gmail.com>
2023-09-20 20:20:06 +0200

    check if user exist in database when creating a new one

---
+++ src/main/java/io/mkolodziejczyk92/anonymoussanta/data/exceptions/UserAlreadyExistException.java
@@ -0,0 +1,8 @@
+package io.mkolodziejczyk92.anonymoussanta.data.exceptions;
+
+public class UserAlreadyExistException extends RuntimeException{
+
+    public UserAlreadyExistException(String message) {
+        super(message);
+    }
+}
